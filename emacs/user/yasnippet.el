f66de7fb77c3963de808a27d9e97c9e8a68abb7f
Liutos <mat.liutos@gmail.com>
2017-07-03 19:24:47 +0800

    在加载目录前检测目录是否存在

--- emacs/user/yasnippet.el
+++ emacs/user/yasnippet.el
@@ -2,4 +2,6 @@
              "~/app/yasnippet-master/")
 (require 'yasnippet)
 (yas-global-mode 1)
-(yas-load-directory "~/.emacs.d/elpa/yasnippet-20161221.1953/snippets")
+(let ((file "~/.emacs.d/elpa/yasnippet-20161221.1953/snippets"))
+  (when (file-exists-p file)
+    (yas-load-directory "~/.emacs.d/elpa/yasnippet-20161221.1953/snippets")))
