2c9db5909e0adfc05463a667a6555d6856c76444
Liutos <mat.liutos@gmail.com>
2017-07-04 00:05:17 +0800

    将命令行的错误重定向到其它位置，绕过该问题

--- emacs/user/builtin.el
+++ emacs/user/builtin.el
@@ -6,7 +6,7 @@
 (setq-default indent-tabs-mode nil)
 
 (defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH' 2> /dev/null")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 (set-exec-path-from-shell-PATH)
