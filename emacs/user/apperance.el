543737e012db16244898e4b22228ad977211fa5c
Liutos <mat.liutos@gmail.com>
2017-07-16 16:39:41 +0800

    设置字体为Source Code Pro

--- emacs/user/apperance.el
+++ emacs/user/apperance.el
@@ -1,4 +1,9 @@
 (electric-pair-mode t)
+
+;; 字体
+(add-to-list 'default-frame-alist '(font . "Source Code Pro"))
+(set-face-attribute 'default t :font "Source Code Pro")
+
 ;; (global-hl-line-mode 1)
 (scroll-bar-mode 1)
 (show-paren-mode 1)
