cee39a54cbf4bb1b5f6c0885b4e27c47403ff51c
Liutos <mat.liutos@gmail.com>
2018-03-18 16:39:44 +0800

    更换字体

--- emacs/user/apperance.el
+++ emacs/user/apperance.el
@@ -1,8 +1,9 @@
 (electric-pair-mode t)
 
 ;; 字体
-(add-to-list 'default-frame-alist '(font . "Monospace"))
-(set-face-attribute 'default t :font "Monospace")
+;; (add-to-list 'default-frame-alist '(font . "Monospace"))
+;; (set-face-attribute 'default t :font "Monospace")
+(set-face-attribute 'default nil :font "Hack" :foundry "simp" :slant 'normal :weight 'normal :height 128 :width 'normal)
 
 ;; (global-hl-line-mode 1)
 (menu-bar-mode 1)
