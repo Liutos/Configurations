a498744e3ac099c6b4efafca1b80c417902b7670
Liutos <mat.liutos@gmail.com>
2019-11-04 08:20:05 +0800

    在部分主模式中自动启用yasnippet

--- emacs/packages/yasnippet.el
+++ emacs/packages/yasnippet.el
@@ -2,4 +2,7 @@
 ;;; package-install yasnippet-snippets
 (require 'yasnippet)
 (yas-reload-all)
+(add-hook 'ledger-mode-hook #'yas-minor-mode)
+(add-hook 'org-mode-hook #'yas-minor-mode)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
+(add-hook 'restclient-mode-hook #'yas-minor-mode)
